df9926dd2e9d3f89ac686adb875dc84e55da83cc
nikkanap <nenaputo@up.edu.ph>
2025-12-28 17:29:18 +0800

    Update README.md

---
+++ my-app/src/pages/HomePage.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './styles.css'
+
+function HomePage() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>SPEEK Messaging Site</h1>
+      <p className="read-the-docs">
+        Welcome to SPEEK, a new chatting website.
+      </p>
+      <a className="link-button" href="/login">
+        Log In
+      </a>
+      <a className="link-button" href="/signup">
+        Sign Up
+      </a>
+    </>
+  )
+}
+
+export default HomePage
---
+++ my-app/src/pages/Login.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function LoginPage() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>SPEEK Messaging Site</h1>
+      <h2>Log In Page</h2>
+      <label>
+        Username
+        <input type="text"></input>
+      </label>
+    </>
+  )
+}
+
+export default LoginPage
