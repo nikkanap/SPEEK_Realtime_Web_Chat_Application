b82dccae5967edf25ef41782cbe6c14b248b602f
nikkanap <nenaputo@up.edu.ph>
2026-01-06 13:52:10 +0800

    Change " to ' in Header file

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -4,12 +4,12 @@ import { Link, useNavigate } from 'react-router-dom';
 function Header() {
 
   return (
-    <div className="header">
+    <div className='header'>
       <h2>SPEEK</h2>
-      <div className="linkContainer">
-        <Link to="/">Home</Link>
-        <Link to="/aboutus">About Us</Link>
-        <Link to="/contact">Contact</Link>
+      <div className='linkContainer'>
+        <Link to='/'>Home</Link>
+        <Link to='/aboutus'>About Us</Link>
+        <Link to='/contact'>Contact</Link>
       </div>
     </div>
   )
