fc07210b33c47d2ffbc1a540c66d14cb85ef6d4f
nikkanap <nenaputo@up.edu.ph>
2026-01-12 22:51:41 +0800

    Create new Header.tsx for userpages

---
+++ frontend/src/userPages/components/Header.tsx
@@ -0,0 +1,18 @@
+import './header.css';
+import { Link, useNavigate } from 'react-router-dom';
+
+function Header() {
+
+  return (
+    <div className='header'>
+      <h2>SPEEK</h2>
+      <div className='linkContainer'>
+        <Link to='/chatlist'>Chat List</Link>
+        <Link to='/notifications'>Notifications</Link>
+        <Link to='/profile'>Profile</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header
